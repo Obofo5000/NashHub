22d4c9e26eb138f2cf553b0a20d15a9afa50fe1b
Obofo5000 <nguyenanhkhoarb79@gmail.com>
2026-06-21 23:25:53 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,12 @@
+local PlaceId = game.PlaceId
+
+if PlaceId == 2753915549 
+or PlaceId == 85211729168715 
+or PlaceId == 4442272183 
+or PlaceId == 79091703265657 
+or PlaceId == 7449423635 
+or PlaceId == 100117331123089 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Obofo5000/NashHub/refs/heads/main/NashHub-BloxFruits.lua"))()
+elseif PlaceId == 97598239454123 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Obofo5000/NashHub/refs/heads/main/NashHub-GAG2.lua"))()
+end
